6f387f0fe61e595efed61cea75f344a3b74cb994
praveen <praveen@belgiumwebnet.com>
2025-10-12 20:13:41 +0000

    update router to hash router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { ProductDetails } from './routes/productDetails';
 import { DiamondsPage } from './routes/diamonds';
 import LandingPage from './components/LandingPage';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <LandingPage />,
