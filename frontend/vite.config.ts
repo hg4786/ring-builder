c9f13b519f2e2b411fab63696f66a285e5609890
praveen <praveen@belgiumwebnet.com>
2025-10-16 11:32:39 +0000

    add separated build for shopify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,24 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../extensions/ring-builder-ui/assets",
-    emptyOutDir: false,
+    outDir: "dist",
+    emptyOutDir: true,
     rollupOptions: {
-      input: path.resolve(__dirname, "src/main.tsx"),
-      output: {
-        entryFileNames: "ring-builder.js",
-        assetFileNames: (assetInfo) => {
-          // Separate images and CSS correctly
-          if (/\.(css)$/.test(assetInfo.name ?? "")) {
-            return "ring-builder.css";
-          }
-          return "assets/[name]-[hash][extname]";
-        },
-      },
+      input: path.resolve(__dirname, "index.html"),
     },
   },
   server: {
